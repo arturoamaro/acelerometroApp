44a6e5a83514c765ebef40784e17787a8e488f01
Arturo Amaro <astark@MacBook-Air-de-Arturo-2.local>
2015-11-28 10:43:19 -0600

    sexta clase

--- js/API/acelerometro.js
+++ js/API/acelerometro.js
@@ -11,9 +11,10 @@ var ac = {
         navigator.accelerometer.clearWatch(watchID);
     },
     onSuccess: function(pos){
+        alert("onSuccess");
         var x = pos.x;
         var y = pos.y;
-        var z = pos.z;alert("onSuccess");
+        var z = pos.z;
         $("#mostrarBru").text("X: "+ x +" Y:"+y);
     },
     onError: function(){
