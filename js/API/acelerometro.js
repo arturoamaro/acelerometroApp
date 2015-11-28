4870e5f8dd6e40dd7e017aae6222fb4e3c68dc9c
Arturo Amaro <astark@MacBook-Air-de-Arturo-2.local>
2015-11-28 11:21:40 -0600

    sexta clase

--- js/API/acelerometro.js
+++ js/API/acelerometro.js
@@ -2,20 +2,23 @@ var ac = {
     watchID: null,
     init: function(){
         var options = { frequency: 500 };  // Update every 3 seconds
-        watchID = navigator.accelerometer.watchAcceleration(ac.listo, ac.onError, options);
+        if(!ac.watchID)
+            watchID = navigator.accelerometer.watchAcceleration(ac.listo, ac.onError, options);
     },
     stop: function(){
-        
-        navigator.accelerometer.clearWatch(watchID);
-        alert("detener");
+        if(ac.watchID != null){
+            navigator.accelerometer.clearWatch(watchID);
+            ac.watchID = null;
+            $("#mostrarBru").text("Acelerometro detenido ");
+        }
     },
     listo: function(pos){
-        var x = pos.x;
-        var y = pos.y;
-        var z = pos.z;
-        $("#mostrarBru").text("X: "+ x +" Y:"+y);
+            var x = pos.x;
+            var y = pos.y;
+            var z = pos.z;
+            $("#mostrarBru").text("X: "+ x +" Y:"+y);
     },
-    onError: function(){
-        alert("Error");
+    onError: function(err){
+        alert(err.code);
     }
 }
--- js/API/brujula.js
+++ js/API/brujula.js
@@ -1,8 +1,22 @@
 var br = {
+    watchId:null,
+    options:{frequency:500},
     init: function(){
-        alert("iniciar");
+        if(!br.watchId)
+            br.watchId = navigator.compass.watchHeading(br.success, br.error, br.options);
     },
     stop: function(){
-        alert("detener");
+        if(br.watchId != null){
+            navigator.compass.clearWatch(br.watchId);
+            br.watchId = null;
+            $("brujula .scroll h2").html("Detenido");
+        }
+    },
+    success: function(h){
+        $("#brujula .scroll h2").html("grados: " +h.magneticHeading);
+        
+    },
+    error: function(err){
+        alert(err.code);
     }
 }
